4f853568074eefd0afb2cd03450369a51652b8a0
Eduardo <eduardogomes.2607@hotmail.com>
2021-02-02 07:16:05 -0300

    tratamento de erros

--- app/Http/Controllers/Api/VendaController.php
+++ app/Http/Controllers/Api/VendaController.php
@@ -4,109 +4,105 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\Produto;
+use Illuminate\Support\Facades\DB;
+
 use App\Models\Cliente;
-use App\Models\Venda;
-use App\Models\ItemVenda;
+
 
 class VendaController extends Controller
 {
-
     /**
      * NOVA VENDA
-     * Verifica se cliente tem cadastro, se nao tiver registra o cliente
+     * Verifica se cliente tem cadastro, se nao tiver, registra o cliente
      * Registra nova venda com referencia ao cliente
      * Registra itens do pedido com referencia a venda
      */
     public function novaVenda(Request $request){
 
-        //Armazenando inputs em variaveis (nome do cliente, email do cliente, pedido, tamanho do pedido, valor total)
-        $clienteNome = $request->input('cliente.nome');
-        $clienteEmail = $request->input('cliente.email');
+        $request->validate([
+            'cliente.nome' => ['required'],
+            'cliente.email' => ['required', 'email'],
+            'pedido' => ['required'],
+            'total' => ['required']
+        ]);
 
-        $pedido = $request->input('pedido');
-        $tamanhoPedido = count($pedido);
+        //Armazenando inputs em variaveis 
+        $inputClienteNome = $request->input('cliente.nome');
+        $inputClienteEmail = $request->input('cliente.email');
+        $inputPedido = $request->input('pedido');
+        $inputTotal = $request->input('total');
 
-        $total = $request->input('total');
-        //---------------------------------------------------
+        //Faz busca do cliente usando o email do cliente informado no input
+        $clienteCadastro = Cliente::where('email', $inputClienteEmail)->first();
 
-        // Faz busca do cliente usando o email do cliente informado no input
-        $clienteCadastro = Cliente::where('email', $clienteEmail)->first();
+        //Se cliente tiver cadastro
+        if($clienteCadastro && ($clienteCadastro->nome == $inputClienteNome)){
+            
+            try{
+                DB::beginTransaction();
 
-        /**
-         * Verifica se o registro foi encontrado e se confere com o nome do cliente
-         * Se sim, registra a venda para o cliente encontrado no banco dados
-         * Se não, registra o cliente no banco de dados e uma venda para o mesmo
-         */
-        if($clienteCadastro && ($clienteCadastro->nome == $clienteNome)){
+                //Registra nova venda fazendo referência ao cliente
+                $newVenda = parent::registraVenda($inputTotal, $clienteCadastro);
+               
+                //Registra itens do pedido fazendo referencia a venda
+                parent::registraItemVenda($inputPedido, $newVenda);
+                
+                DB::commit();
+                return response()->json(
+                    [
+                        'message' => "Pedido realizado com sucesso! Entraremos em contato através do e-mail com as instruções de pagamento. Obrigado!",
+                        'cliente' => $clienteCadastro,
+                        'venda' => $newVenda
+                    ]
+                );
 
-            //Registra venda para o cliente
-            $venda = new Venda;
-            $venda->valor = $request->input('total');
-            $venda->status = null;
-            $venda->id_cliente = $clienteCadastro->id;
-            $venda->save();
-            //----------------------------------------
-
-            // Percorre o array de pedido e registra um itemVenda para cada item 
-            for($i = 0; $i < $tamanhoPedido; $i++){
-                $item = new ItemVenda;
-                $item->preco = $pedido[$i]['preco'];
-                $item->id_produto = $pedido[$i]['id'];
-                $item->id_venda = $venda->id;
-                $item->save();
+            }catch(\Exception $e){
+                DB::rollBack();
+                return response()->json(
+                    [
+                        'message' => "Erro ao finalizar seu pedido: Tente novamente e entre em contato conosco, se o erro persistir",
+                        'erro' => $e
+                    ]
+                );
             }
-            //-------------------------------------------------
-
-            return response()->json(
-                [
-                    'cliente' => $clienteCadastro,
-                    'venda' => $venda,
-                    'item' => $item
-
-                ]
-            );
-
+            
+        //Se cliente nao for cadastrado    
         }else{
             
-            //Registra cliente
-            $cliente = new Cliente;
-            $cliente->nome = $clienteNome;
-            $cliente->email = $clienteEmail;
-            $cliente->save();
-            //---------------------------------
+            try{
+                DB::beginTransaction();
 
-            //Registra venda para o cliente
-            $venda = new Venda;
-            $venda->valor = $request->input('total');
-            $venda->status = null;
-            $venda->id_cliente = $cliente->id;
-            $venda->save();
-            //----------------------------------------
+                //Registra cliente
+                $newCliente = parent::registraCliente($inputClienteNome, $inputClienteEmail);
 
-            // Percorre o array de pedido e registra um itemVenda para cada item 
-            for($i = 0; $i < $tamanhoPedido; $i++){
-                $item = new ItemVenda;
-                $item->preco = $pedido[$i]['preco'];
-                $item->id_produto = $pedido[$i]['id'];
-                $item->id_venda = $venda->id;
-                $item->save();
+                //Registra venda para o novo cliente  
+                $newVenda = parent::registraVenda($inputTotal, $newCliente);
+
+                //Registra itens do pedido fazendo referencia a venda
+                parent::registraItemVenda($inputPedido, $newVenda);
+
+                DB::commit();
+                return response()->json(
+                    [
+                        'message' => "Pedido realizado com sucesso! Entraremos em contato através do e-mail com as instruções de pagamento. Obrigado!",
+                        'cliente' => $newCliente,
+                        'venda' => $newVenda
+                    ]
+                );
+
+            }catch(\Exception $e){
+                DB::rollBack();
+                return response()->json(
+                    [
+                        'message' => "Erro ao finalizar seu pedido: Tente novamente e entre em contato conosco, se o erro persistir",
+                        'erro' => $e
+                    ]
+                );
             }
-            //-------------------------------------------------
-
-            return response()->json(
-                [
-                    'cliente' => $cliente,
-                    'venda' => $venda,
-                    'item' => $item
-
-                ]
-            );
         }
-        //Fim verificacao de cliente e registro de nova venda------------------
-
+        //Fim verificacao de cliente e registro de nova venda
     }
-
+    //Fim novaVenda
 
     /**
      * Display the specified resource.
@@ -115,18 +111,6 @@ class VendaController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
     {
         //
     }
--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,7 +7,41 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+use App\Models\Cliente;
+use App\Models\Venda;
+use App\Models\ItemVenda;
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    //Registra Cliente e retorna o registro criado
+    public function registraCliente($clienteNome, $clienteEmail){
+        $cliente = new Cliente;
+        $cliente->nome = $clienteNome;
+        $cliente->email = $clienteEmail;
+        $cliente->save();
+        return $cliente;
+    }
+
+    //registra venda e retorna venda criada
+    public function registraVenda($total, $cliente){
+        $venda = new Venda;
+        $venda->valor = $total;
+        $venda->status = "Aguardando pagamento";
+        $venda->id_cliente = $cliente->id;
+        $venda->save();
+        return $venda;
+    }
+
+    //registra cada item da venda
+    public function registraItemVenda($pedido, $newVenda){
+        foreach($pedido as $item){
+            $itemVenda = new ItemVenda;
+            $itemVenda->preco = $item['preco'];
+            $itemVenda->id_produto =  $item['id'];
+            $itemVenda->id_venda = $newVenda->id;
+            $itemVenda->save();
+        }
+    }
 }
